ffbbcc4eb8d47722e80921d7c6b69e0af1c37cff
faefey <firewolfiepup@gmail.com>
2024-09-24 15:11:36 -0400

    stylized selected file menu and added icons

--- client/src/components/selectedfilemenu.js
+++ client/src/components/selectedfilemenu.js
@@ -1,6 +1,9 @@
 import {useState} from 'react'
 
-// I plan on stylizing this later when everything is combined into one page
+import {ReactComponent as Download} from '../icons/download.svg'
+import {ReactComponent as Delete} from '../icons/delete.svg'
+import {ReactComponent as Info} from '../icons/info.svg'
+
 export default function SelectedFileMenu() {
     const [selectedFiles, setSelectedFiles] = useState([])
 
@@ -21,21 +24,21 @@ function FileFilters() {
     return (
         <div id="filefilters">
             
-            <select id="typefilter" defaultValue="Type">
+            <select id="typefilter" className="filter" defaultValue="Type">
                 <option disabled hidden>Type</option>
                 <option value="document">Document</option>
                 <option value="media">Media</option>
                 <option value="other">Other</option>
             </select>
 
-            <select id="sizefilter" defaultValue="Size">
+            <select id="sizefilter" className="filter" defaultValue="Size">
                 <option disabled hidden>Size</option>
                 <option value="less1gb">{"<"} 1 GB</option>
                 <option value="1to5gb">1 - 5 GB</option>
                 <option value="more5gb">{">"} 5 GB</option>
             </select>
 
-            <select id="datefilter" defaultValue="Date">
+            <select id="datefilter" className="filter" defaultValue="Date">
                 <option disabled hidden>Date</option>
                 <option value="today">Today</option>
                 <option value="7days">Last 7 days</option>
@@ -45,21 +48,21 @@ function FileFilters() {
                 <option value="lastyear">Last year</option>
             </select>
 
-            <select id="downloadfilter" defaultValue="Downloads">
+            <select id="downloadfilter" className="filter" defaultValue="Downloads">
                 <option disabled hidden>Downloads</option>
                 <option value="less100">{"<"} 100</option>
                 <option value="100to1000">100 - 1000</option>
                 <option value="more1000">{">"} 1000</option>
             </select>
 
-            <select id="pricefilter" defaultValue="Price">
+            <select id="pricefilter" className="filter" defaultValue="Price">
                 <option disabled hidden>Price</option>
                 <option value="less1">{"<"} 1</option>
                 <option value="1to2">1 - 2</option>
                 <option value="more2">{">"} 2</option>
             </select>
 
-            <button onClick={clearFilters}>Clear Filters</button>
+            <button id="clearfilters" className="filter" onClick={clearFilters}>Clear Filters</button>
         </div>
     )
 }
@@ -68,9 +71,9 @@ function FileActions({selectedFiles}) {
     return (
         <div id="fileactions">
             <p style={{display: "inline"}}>{selectedFiles.length} selected</p>
-            <span className="material-symbols-outlined">download</span>
-            <span className="material-symbols-outlined">delete</span>
-            <span className="material-symbols-outlined">info</span>
+            <Download className="icon" />
+            <Delete className="icon" />
+            <Info className="icon" />
         </div>
     )
 }
