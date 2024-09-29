038cf95d55e9457ab58a6d2df18279d951c553ac
John Roedel <john.roedel@stonybrook.edu>
2024-09-29 16:59:51 -0400

    Increased the beauty of the popup menu

--- client/src/components/HostPopup.js
+++ client/src/components/HostPopup.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import Popup from 'reactjs-popup';
 
+const labelStyle = {display: 'flex',
+                    justifyContent: "space-between",
+                    margin: "10px"
+                   };
+
+const popupBorder = {border: "2px solid black"};
+
+const popupText = {fontWeight: 'bold', fontSize: '25px'};
+
+//const formBox = { display: "flex", flexDirection: "column", alignItems: "center" };
+
 /*
     Button that is displayed only when the hosted files are shown
     When clicked, a popup is prompted
@@ -26,24 +37,31 @@ export default function HostPopup() {
     
     return (
         <Popup  trigger={<button>Add file</button>}
-                position={['bottom center', 'top center']}
-                closeOnDocumentClick={false}>
+                position={['left']}
+                contentStyle={{ width: '400px', height: '300px' }}
+                closeOnDocumentClick={false} modal>
 
             {(close) => (
-            <form onSubmit={(event) => inputData(event, close)}>
-                <label>File name: </label>
-                <input type="text" name = "filename" placeholder="Search files..."/>
-                <br />
-                <br />
-                <label>File price: </label>
-                <input type="text" name = "fileprice" placeholder="Value"/>
-                <br />
-                <br />
-                <button type="submit">
-                    Add file
-                </button>
+            <div style={popupBorder}>
+                <form onSubmit={(event) => inputData(event, close)}>
+                    <div style={labelStyle}>
+                        <label style={popupText}>File name: </label>
+                        <input type="text" name = "filename" placeholder="Search files..."/>
+                    </div>
+                    <br />
+                    <br />
+                    <div style={labelStyle}>
+                        <label style={popupText}>File price: </label>
+                        <input type="text" name = "fileprice" placeholder="Value"/>
+                    </div>
+                    <br />
+                    <br />
+                    <button type="submit" style={{alignSelf: "flex-center"}}>
+                        Add file
+                    </button>
 
-            </form>
+                </form>
+            </div>
             )}
         </Popup>
     );
