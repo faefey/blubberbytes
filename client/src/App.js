f30fea3e0fd97d0171dbafb19d9e26da21c84a64
Jazz Kaur <jazz.kaur@stonybrook.edu>
2024-09-29 15:22:38 -0400

    original App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,10 @@
-import Table from "./components/listFile/Table";
-import tableData1 from "./data/tableData1.json";
-import "./stylesheets/table.css";
-const columns = [
-  { label: "File Name", accessor: "FileName", sortable: true },
-  { label: "FileSize", accessor: "FileSize", sortable: true },
-  { label: "DateListed", accessor: "DateListed", sortable: true },
-  { label: "downloads", accessor: "downloads", sortable: true },
-];
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <div className="table_container">
-      <Table
-        data={tableData1}
-        columns={columns}
-      />
+    <div className="App">
     </div>
   );
-};
+}
 
 export default App;
