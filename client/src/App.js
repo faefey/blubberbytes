a1789259f73354e89afea1c4a0bf1fdf084751bc
John Roedel <john.roedel@stonybrook.edu>
2024-09-29 15:52:23 -0400

    Banner test

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
-import './App.css';
+import React from 'react';
+import {useState} from 'react';
+
+import './stylesheets/App.css';
+
+import Banner from './components/Banner.js';
+import SideMenu from './components/sideMenu.jsx';
+import {fileItems, tagItems} from './components/menuItems.jsx';
+import SelectedFileMenu from './components/selectedfilemenu.js';
+import Table from "./components/listFile/Table.js";
+
+import tableData1 from "./data/tableData1.json";
+
+const columns = [ { label: "File Name", accessor: "FileName", sortable: true }, { label: "FileSize", accessor: "FileSize", sortable: true }, { label: "DateListed", accessor: "DateListed", sortable: true }, { label: "downloads", accessor: "downloads", sortable: true }, ];
 
 function App() {
+  const [currPage, setCurrPage] = useState(0);
+
   return (
     <div className="App">
+	    <Banner />
+      <SideMenu fileItems={fileItems} tagItems={tagItems} />
+      <SelectedFileMenu />
+      <Table data={tableData1} columns={columns} />
     </div>
   );
 }
--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -1,5 +1,8 @@
 import SearchBar from ".//SearchBar.js";
 import logo from '../bb-logo.png';
+import './SettingsPage.css';
+
+import { useState } from 'react';
 
 const headerStyle = {
     display: 'flex',            
@@ -11,7 +14,15 @@ const headerStyle = {
 
 const imageStyle = { width: '500px', height: 'auto' };
 
+//setCurrentPage and currPage
+//{setCurrPage, currPage}
 export default function Banner() {
+    const [isDropdownVisible, setIsDropdownVisible] = useState(false); // Dropdown state
+
+    const toggleDropdown = () => {
+        setIsDropdownVisible(!isDropdownVisible); // Toggle dropdown visibility
+    };
+
     return (
     <header id = "myhead" style={headerStyle}>
         <div className="logo-container">
@@ -20,7 +31,20 @@ export default function Banner() {
                  style={imageStyle} />
         </div>
         <SearchBar/>
-        <div></div>
+        <div className="profile-button-container">
+          <button className="profile-button" onClick={toggleDropdown}>
+            <img src="/profile-icon.png" alt="Profile" />
+          </button>
+          {/* Dropdown for Log Out */}
+          {isDropdownVisible && (
+            <div className="dropdown">
+              <button className="dropdown-item">Log Out</button>
+              <button className="dropdown-item">Settings</button>
+            </div>
+          )}
+        </div>
     </header>
     );
 }
+
+// <button onClick={() => setCurrPage(1)}>Go to settings</button>
