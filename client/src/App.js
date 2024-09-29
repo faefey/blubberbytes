674318b33470e825ce1bcb7007f00ae4a9701cfa
Sahibjot Bhullar <sahib.bhu7111@gmail.com>
2024-09-29 15:17:30 -0400

    css

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import Table from "./components/listFile/Table";
+import tableData1 from "./data/tableData1.json";
+import "./stylesheets/table.css";
+const columns = [
+  { label: "File Name", accessor: "FileName", sortable: true },
+  { label: "FileSize", accessor: "FileSize", sortable: true },
+  { label: "DateListed", accessor: "DateListed", sortable: true },
+  { label: "downloads", accessor: "downloads", sortable: true },
+];
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <div className="table_container">
+      <Table
+        data={tableData1}
+        columns={columns}
+      />
     </div>
   );
-}
+};
 
 export default App;
