2bc297efb590f8916e7766d04f471a4aa8a09c04
faefey <firewolfiepup@gmail.com>
2024-10-09 13:57:58 -0400

    put back states

--- client/src/components/mainContent.js
+++ client/src/components/mainContent.js
@@ -1,66 +1,38 @@
-import {useState} from 'react';
-
 import SideMenu from './sideMenu.js';
 import SelectedFileMenu from './selectedfilemenu.js';
 import Table from "./Table.js";
-
-import tableData1 from "../data/tableData1.json";
-
-
 
 import {ReactComponent as Hosting} from '../icons/server.svg'
 import {ReactComponent as Purchased} from '../icons/paid.svg'
 import {ReactComponent as Sharing} from '../icons/folder.svg'
 import {ReactComponent as Explore} from '../icons/globe1.svg'
 
-export default function MainContent({data, columns}) {
-  const [currSection, setCurrSection] = useState('Hosting')
-
+export default function MainContent({data, columns, currSection, currShownData, updateShownData}) {
   const fileItems = [
     {
       label: 'Hosting', icon: <Hosting />,
-      onClick: () => setCurrSection('Hosting')
+      onClick: () => updateShownData('Hosting')
     },
     {
       label: 'Purchased', icon: <Purchased />,
-      onClick: () => setCurrSection('Purchased')
+      onClick: () => updateShownData('Purchased')
     },
     {
       label: 'Sharing', icon: <Sharing />,
-      onClick: () => setCurrSection('Sharing')
+      onClick: () => updateShownData('Sharing')
     },
     {
       label: 'Explore', icon: <Explore />,
-      onClick: () => setCurrSection('Explore')
+      onClick: () => updateShownData('Explore')
     }
   ];
-  const [filters, setFilters] = useState({
-    type: "",
-    size: "",
-    date: "",
-    downloads: "",
-    price: "",
-  })
-  ;
-  const [selectedFiles, setSelectedFiles] = useState([]);
-
-
+  
   return (
       <div className="maincontent">
         <SideMenu items={fileItems} currSection={currSection} />
         <div className="content">
-        <SelectedFileMenu
-          filters={filters}
-          setFilters={setFilters}
-          selectedFiles={selectedFiles}
-        />
-        <Table
-          data={tableData1}
-          columns={columns}
-          filters={filters}
-          selectedFiles={selectedFiles}
-          setSelectedFiles={setSelectedFiles}
-        />
+          <SelectedFileMenu />
+          <Table data={currShownData} columns={columns} />
         </div>
       </div>
   );
