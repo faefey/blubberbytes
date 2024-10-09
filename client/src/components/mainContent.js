19614867ad3362acdd8ee21e3bb6da65f435ef61
Sahibjot Bhullar <56088617+sbhullar711@users.noreply.github.com>
2024-10-09 14:15:46 -0400

    Update mainContent.js

--- client/src/components/mainContent.js
+++ client/src/components/mainContent.js
@@ -1,39 +1,69 @@
+// mainContent.js
+import { useState } from 'react';
+
 import SideMenu from './sideMenu.js';
-import SelectedFileMenu from './selectedfilemenu.js';
 import Table from "./Table.js";
 
-import {ReactComponent as Hosting} from '../icons/server.svg'
-import {ReactComponent as Purchased} from '../icons/paid.svg'
-import {ReactComponent as Sharing} from '../icons/folder.svg'
-import {ReactComponent as Explore} from '../icons/globe1.svg'
+import tableData1 from "../data/tableData1.json"; 
+import tableData2 from "../data/tableData2.json"; 
+import tableData3 from "../data/tableData3.json"; 
+import tableData4 from "../data/tableData4.json"; 
 
-export default function MainContent({data, columns, currSection, currShownData, updateShownData}) {
+import { ReactComponent as Hosting } from '../icons/server.svg';
+import { ReactComponent as Purchased } from '../icons/paid.svg';
+import { ReactComponent as Sharing } from '../icons/folder.svg';
+import { ReactComponent as Explore } from '../icons/globe1.svg';
+
+export default function MainContent({ columns }) {
+  const [currSection, setCurrSection] = useState('Hosting');
+
   const fileItems = [
     {
-      label: 'Hosting', icon: <Hosting />,
-      onClick: () => updateShownData('Hosting')
+      label: 'Hosting',
+      icon: <Hosting />,
+      onClick: () => setCurrSection('Hosting'),
     },
     {
-      label: 'Purchased', icon: <Purchased />,
-      onClick: () => updateShownData('Purchased')
+      label: 'Purchased',
+      icon: <Purchased />,
+      onClick: () => setCurrSection('Purchased'),
     },
     {
-      label: 'Sharing', icon: <Sharing />,
-      onClick: () => updateShownData('Sharing')
+      label: 'Sharing',
+      icon: <Sharing />,
+      onClick: () => setCurrSection('Sharing'),
     },
     {
-      label: 'Explore', icon: <Explore />,
-      onClick: () => updateShownData('Explore')
-    }
+      label: 'Explore',
+      icon: <Explore />,
+      onClick: () => setCurrSection('Explore'),
+    },
   ];
-  
+
+  const getTableData = () => {
+    switch (currSection) {
+      case 'Hosting':
+        return tableData1;
+      case 'Purchased':
+        return tableData2;
+      case 'Sharing':
+        return tableData3;
+      case 'Explore':
+        return tableData4;
+      default:
+        return tableData1;
+    }
+  };
+
   return (
-      <div className="maincontent">
-        <SideMenu items={fileItems} currSection={currSection} />
-        <div className="content">
-          <SelectedFileMenu />
-          <Table data={currShownData} columns={columns} />
-        </div>
+    <div className="maincontent">
+      <SideMenu items={fileItems} currSection={currSection} />
+      <div className="content">
+        <Table
+          data={getTableData()}
+          columns={columns}
+        />
       </div>
+    </div>
   );
 }
