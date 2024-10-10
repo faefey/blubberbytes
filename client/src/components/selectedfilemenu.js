587936248ea87156f0aa23ec2f946db24de741cd
faefey <firewolfiepup@gmail.com>
2024-10-09 20:26:01 -0400

    remove filter name from dropdown options

--- client/src/components/selectedfilemenu.js
+++ client/src/components/selectedfilemenu.js
@@ -37,7 +37,7 @@ function FileFilters({ filters, setFilters }) {
         value={filters.type}
         onChange={(e) => setFilters({ ...filters, type: e.target.value })}
       >
-        <option value="">Type</option>
+        <option value="" hidden>Type</option>
         <option value="document">Document</option>
         <option value="media">Media</option>
         <option value="other">Other</option>
@@ -49,7 +49,7 @@ function FileFilters({ filters, setFilters }) {
         value={filters.size}
         onChange={(e) => setFilters({ ...filters, size: e.target.value })}
       >
-        <option value="">Size</option>
+        <option value="" hidden>Size</option>
         <option value="less1gb">{'<'} 1 GB</option>
         <option value="1to5gb">1 - 5 GB</option>
         <option value="more5gb">{'>'} 5 GB</option>
@@ -61,7 +61,7 @@ function FileFilters({ filters, setFilters }) {
         value={filters.date}
         onChange={(e) => setFilters({ ...filters, date: e.target.value })}
       >
-        <option value="">Date</option>
+        <option value="" hidden>Date</option>
         <option value="today">Today</option>
         <option value="7days">Last 7 days</option>
         <option value="30days">Last 30 days</option>
@@ -76,7 +76,7 @@ function FileFilters({ filters, setFilters }) {
         value={filters.downloads}
         onChange={(e) => setFilters({ ...filters, downloads: e.target.value })}
       >
-        <option value="">Downloads</option>
+        <option value="" hidden>Downloads</option>
         <option value="less100">{'<'} 100</option>
         <option value="100to1000">100 - 1000</option>
         <option value="more1000">{'>'} 1000</option>
@@ -88,7 +88,7 @@ function FileFilters({ filters, setFilters }) {
         value={filters.price}
         onChange={(e) => setFilters({ ...filters, price: e.target.value })}
       >
-        <option value="">Price</option>
+        <option value="" hidden>Price</option>
         <option value="less1">{'<'} 1</option>
         <option value="1to2">1 - 2</option>
         <option value="more2">{'>'} 2</option>
