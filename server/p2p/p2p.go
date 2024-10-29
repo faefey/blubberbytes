464865a00bea618ea07ba7bcd42af7bfb3fa8795
CillianRen <qilong2002@gmail.com>
2024-10-28 21:42:36 -0400

    Update p2p.go to prompt user for Student ID input

--- server/p2p/p2p.go
+++ server/p2p/p2p.go
@@ -7,13 +7,19 @@ import (
 )
 
 var (
-	node_id             = "114369970" // give your SBU ID
+	node_id             string
 	relay_node_addr     = "/ip4/130.245.173.221/tcp/4001/p2p/12D3KooWDpJ7As7BWAwRMfu1VU2WCqNjvq387JEYKDBj4kx6nXTN"
 	bootstrap_node_addr = "/ip4/130.245.173.222/tcp/61000/p2p/12D3KooWQd1K1k8XA9xVEzSAu7HUCodC7LJB6uW5Kw4VwkRdstPE"
 	globalCtx           context.Context
 )
 
 func P2P() {
+	fmt.Print("Enter your student ID: ")
+	_, err := fmt.Scanln(&node_id)
+	if err != nil {
+		log.Fatalf("Failed to read student ID: %s", err)
+	}
+
 	node, dht, err := createNode()
 	if err != nil {
 		log.Fatalf("Failed to create node: %s", err)
@@ -25,9 +31,10 @@ func P2P() {
 
 	fmt.Println("Node multiaddresses:", node.Addrs())
 	fmt.Println("Node Peer ID:", node.ID())
+	fmt.Println("Your Student ID:", node_id)
 
 	connectToPeer(node, relay_node_addr)     // connect to relay node
-	makeReservation(node)                    // make reservation on realy node
+	makeReservation(node)                    // make reservation on relay node
 	connectToPeer(node, bootstrap_node_addr) // connect to bootstrap node
 	go handlePeerExchange(node)
 	go receiveDataFromPeer(node, "D:/blubberbytes/cse416-dht-go-main/") // Ensures a folder path is used
