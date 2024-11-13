d8cf7f476d84f6babcfd4eacc13b2e286235dd0e
John Roedel <john.roedel@stonybrook.edu>
2024-11-12 20:01:38 -0500

    Added function to hash files

---
+++ server/hash.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"os"
+)
+
+// Takes a file at located filePath returns the sha256 encoding of its contents in hex form
+// To test it out, try hashing cmd.txt and then finding the hash checksum here:
+// https://emn178.github.io/online-tools/sha256_checksum.html
+func hashFile(filePath string) (string, error) {
+	fileContent, err := os.Open(filePath)
+	if err != nil {
+		return "Error hashing", err
+	}
+	defer fileContent.Close()
+
+	h := sha256.New()
+	if _, err := io.Copy(h, fileContent); err != nil {
+		return "Error hashing", err
+	}
+
+	hash := hex.EncodeToString(h.Sum(nil))
+	fmt.Println("Hash of file at " + filePath + ": " + hash)
+	return hex.EncodeToString(h.Sum(nil)), nil
+}
