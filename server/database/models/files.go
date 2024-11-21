ab8ef971a96a5d7869a0b150118767f7fc863c8e
faefey <firewolfiepup@gmail.com>
2024-11-21 17:24:17 -0500

    database models

---
+++ server/database/models/files.go
@@ -0,0 +1,31 @@
+package database
+
+// Table for added files
+type Storing struct {
+	Hash      string `json:"hash"`
+	Name      string `json:"name"`
+	Extension string `json:"extension"`
+	Size      int64  `json:"size"`
+	Path      string `json:"path"`
+	Date      string `json:"date"`
+}
+
+// Join on file hash with Storing table
+type Hosting struct {
+	Hash  string  `json:"hash"`
+	Price float64 `json:"price"`
+}
+
+// Join on file hash with Storing table
+type Sharing struct {
+	Hash     string `json:"hash"`
+	Password string `json:"password"`
+}
+
+// Table for saved files
+type Saved struct {
+	Hash      string `json:"hash"`
+	Name      string `json:"name"`
+	Extension string `json:"extension"`
+	Size      int64  `json:"size"`
+}
---
+++ server/database/models/histories.go
@@ -0,0 +1,33 @@
+package database
+
+// Table for upload history
+type Uploads struct {
+	Date      string `json:"date"`
+	Name      string `json:"name"`
+	Extension string `json:"extension"`
+	Size      int64  `json:"size"`
+	Hash      string `json:"hash"`
+}
+
+// Table for download history
+type Downloads struct {
+	Date      string  `json:"date"`
+	Name      string  `json:"name"`
+	Extension string  `json:"extension"`
+	Size      int64   `json:"size"`
+	Price     float64 `json:"price"`
+	Hash      string  `json:"hash"`
+}
+
+// Table for transaction history
+type Transactions struct {
+	Date    string  `json:"date"`
+	Wallet  string  `json:"wallet"`
+	Amount  float64 `json:"amount"`
+	Balance float64 `json:"balance"`
+}
+
+// Put this on hold until proxies are implemented
+// Table for proxy history
+type Proxies struct {
+}
