44470d5759afb982104314be900ca019fd0cfec2
Jazz Kaur <kaur3@cs.stonybrook.edu>
2024-11-25 21:35:03 -0500

    miscellaneous

--- server/database/operations/transactions.go
+++ server/database/operations/transactions.go
@@ -3,6 +3,7 @@ package operations
 import (
 	"database/sql"
 	"fmt"
+	"server/database/models"
 )
 
 func AddTransactions(db *sql.DB, id int64, date, wallet string, amount, balance float64) error {
@@ -10,9 +11,30 @@ func AddTransactions(db *sql.DB, id int64, date, wallet string, amount, balance 
 	          VALUES (?, ?, ?, ?, ?)`
 	_, err := db.Exec(query, id, date, wallet, amount, balance)
 	if err != nil {
-		return fmt.Errorf("error adding record to Transactions: %v", err)
+		return fmt.Errorf("error adding record to Transaction History: %v", err)
 	}
 
-	fmt.Printf("Record added to Transactions with id: %d\n", id)
+	fmt.Printf("Record added to Transaction History with id: %d\n", id)
 	return nil
+}
+
+func GetAllTransactions(db *sql.DB) ([]models.Transactions, error) {
+	query := `SELECT id, date, wallet, amount, balance FROM Transactions`
+	rows, err := db.Query(query)
+	if err != nil {
+		return nil, fmt.Errorf("error querying Transaction History table: %v", err)
+	}
+	defer rows.Close()
+
+	var transactionsRecords []models.Transactions
+	for rows.Next() {
+		var record models.Transactions
+		err := rows.Scan(&record.Id, &record.Date, &record.Wallet, &record.Amount, &record.Balance)
+		if err != nil {
+			return nil, fmt.Errorf("error scanning Transaction History record: %v", err)
+		}
+		transactionsRecords = append(transactionsRecords, record)
+	}
+
+	return transactionsRecords, nil
 }
